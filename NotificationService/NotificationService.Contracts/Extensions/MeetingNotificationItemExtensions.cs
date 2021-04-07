1e865c4107bdc2cf97d4ad1ec34eabec94bd628d
Sanjay Prajapati <saprajap@microsoft.com>
2021-04-07 19:45:22 +0530

    meeting invite issue fix of templateData and missing tracking id in response

--- NotificationService/NotificationService.Contracts/Extensions/MeetingNotificationItemExtensions.cs
+++ NotificationService/NotificationService.Contracts/Extensions/MeetingNotificationItemExtensions.cs
@@ -56,6 +56,7 @@ namespace NotificationService.Contracts.Extensions
                     TemplateId = meetingNotificationItem.TemplateId,
                     TemplateData = meetingNotificationItem.TemplateData,
                     Application = applicationName,
+                    TrackingId = meetingNotificationItem.TrackingId,
                     Action = meetingNotificationItem.Action,
                 };
             }
