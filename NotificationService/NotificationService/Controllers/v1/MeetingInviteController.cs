55fa7cb23de2b5faaca89318ceef68fd08443144
deepika791 <desomaga@microsoft.com>
2021-04-20 17:51:03 +0530

    controller error fix

--- NotificationService/NotificationService/Controllers/v1/MeetingInviteController.cs
+++ NotificationService/NotificationService/Controllers/v1/MeetingInviteController.cs
@@ -24,7 +24,7 @@ namespace NotificationService.Controllers
     [Route("v1/meetinginvite")]
     [Authorize(Policy = ApplicationConstants.AppNameAuthorizePolicy)]
     [ServiceFilter(typeof(ValidateModelAttribute))]
-    public class MeetingInviteController
+    public class MeetingInviteController : Controller
     {
         /// <summary>
         /// Instance of <see cref="IEmailManager"/>.
